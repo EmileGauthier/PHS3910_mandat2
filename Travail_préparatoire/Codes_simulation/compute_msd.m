97e02382e23fab86d73107d6672859110a1f72d3
Laurent Dupuis <143975790+ladupf@users.noreply.github.com>
2025-10-28 21:42:16 -0400

    Comments
    Added input description for positions and max_lag.

--- Travail_préparatoire/Codes_simulation/compute_msd.m
+++ Travail_préparatoire/Codes_simulation/compute_msd.m
@@ -3,13 +3,14 @@
 
 function results_msd = compute_msd(positions, max_lag)
 % fonction créée par chatgpt puis modifiée par l'équipe.
+% Input : positions (une matrice N par 2 contenant les coordonnées de tous les fits gaussiens, pour une marche aléatoire complète).
+%         max_lag (saut maximal entre frames qu'on souhaite évaluer)
 % Output : results_msd = [taus, msd], deux vecteur colonnes.
-
 % taus : vecteur contenant les entiers des lag times, allant de 1 à max_lag
 % (correspond à l'axe x du graphique de l'énoncé)
 % msd : vecteur contenant les valeurs du msd, correspond aux valeurs y du
 % graphique de l'énoncé.
-% positions : matrice N par 2 (dans l'unité donnée, ex m ou um)
+
 N = size(positions,1); % On a N points dans la trajectoire brownienne
 if nargin < 2 || isempty(max_lag) % Si on donne pas de valeur de max_lag, on le fixe à N/4, pourrait etre jusqu'à floor(N/2). 
     max_lag = floor(N/4);
