3a8aef48bd0d965fb627aac8b751d882fb643e76
Laurent Dupuis <143975790+ladupf@users.noreply.github.com>
2025-10-28 21:33:59 -0400

    Implement compute_msd function for MSD calculation
    Function computes the mean squared displacement (MSD) from given positions over specified lag times.

---
+++ Travail_préparatoire/Codes_simulation/compute_msd.m
@@ -0,0 +1,26 @@
+% Étape 4 - Avec toutes les positions sur toutes les photos, calculer le
+% MSD
+
+function results_msd = compute_msd(positions, max_lag)
+% taus : vecteur contenant les entiers des lag times, allant de 1 à max_lag
+% (correspond à l'axe x du graphique de l'énoncé)
+% msd : vecteur contenant les valeurs du msd, correspond aux valeurs y du
+% graphique de l'énoncé.
+% positions : matrice N par 2 (dans l'unité donnée, ex m ou um)
+N = size(positions,1); % On a N points dans la trajectoire brownienne
+if nargin < 2 || isempty(max_lag) % Si on donne pas de valeur de max_lag, on le fixe à N/4, pourrait etre jusqu'à floor(N/2). 
+    max_lag = floor(N/4);
+end
+msd = zeros(max_lag,1); % Vecteur colonne
+
+for lag = 1:max_lag % Sur sur les times lags (delta_t dans l'énoncé)
+    diffs = positions(lag+1:end,:) - positions(1:end-lag,:); 
+    % 1er terme : positions au temps futur, 2e terme : positions initiales associées. 
+    % Diffs est une matrice (N - lag) x 2
+    sq = sum(diffs.^2,2); % Déplacements quadratiques sommés en x et y. 
+    msd(lag) = mean(sq);
+end
+taus = (1:max_lag)'; % Vecteur colonne
+
+results_msd = [taus, msd];
+end
